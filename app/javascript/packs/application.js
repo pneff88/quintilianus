d061899eb62ce6ebdca4d9a3301f30be53ee6001
patrick neff <pneff88@gmail.com>
2022-07-02 09:34:18 -0400

    fixed navbar problem from vocab builder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,8 @@
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
+if (!ReactRailsUJS) {
+    location.reload();
+}
 ReactRailsUJS.useContext(componentRequireContext);
+
--- app/javascript/packs/verble.jsx
+++ app/javascript/packs/verble.jsx
@@ -8,6 +8,7 @@ import App from "../src/App.js"
 let hasloaded = false;
 
 document.addEventListener('DOMContentLoaded', () => {
+  console.log("************")
   if (!hasloaded) {
   ReactDOM.render(
       <App/>,
