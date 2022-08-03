e44fb1cf9084ca69055843b71512392d497e49d7
patrick neff <pneff88@gmail.com>
2022-08-03 17:34:01 -0400

    added admin column with default false to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_212323) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_212446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_212323) do
     t.integer "pecunia"
     t.string "nomen"
     t.string "aboutme"
+    t.boolean "admin", default: false
   end
 
   create_table "words", force: :cascade do |t|
