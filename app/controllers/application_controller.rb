62d11c61b8851ffe9fa4bb596bf049d36583e12e
patrick neff <pneff88@gmail.com>
2022-08-07 12:46:43 -0400

    sessions set to thirty minutes with expiration in the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
     before_action :set_current_user
+    before_action :log_out_expired
 
     def set_current_user
         if session[:user_id] 
             Current.user = User.find_by(id: session[:user_id])
+        end
+    end
+
+    def log_out_expired
+        if session[:expires_at]
+            if session[:expires_at] < Time.current
+                session.destroy
+                redirect_to sign_in_path, alert: "Session expired."
+            end
         end
     end
 
@@ -12,5 +22,7 @@ class ApplicationController < ActionController::Base
             redirect_to sign_in_path, alert: "You must be signed in to do that."
         end
     end
+
+
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
         if user.present? && user.authenticate(params[:password])
             session[:user_id]=user.id
-            session[:expires_at] = Time.current + 1.minute
+            session[:expires_at] = Time.current + 30.minutes
             redirect_to root_path, notice: "Successfully logged in."
         else
             redirect_to sign_in_path, alert: "Invalid email or password."
